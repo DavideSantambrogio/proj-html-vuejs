84eca599b569bddca74f09c6307a5f2f312bb775
DavideSantambrogio <santambrogio.davide.97@gmail.com>
2023-12-04 12:35:23 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  
+}
+
+</script>
+
+<template>
+ 
+</template>
+
+<style scoped>
+/* Importazione di stili generali e icone FontAwesome */
+  @use "./style/general.scss";
+  @import "@fortawesome/fontawesome-free/css/all.css"; 
+
+</style>
